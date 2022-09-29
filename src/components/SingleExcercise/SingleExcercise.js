576790beb77dd700661e42373de32a83d3334580
theMorshed <themorshedctg@gmail.com>
2022-09-29 08:34:36 +0600

    display all excercise

--- src/components/SingleExcercise/SingleExcercise.js
+++ src/components/SingleExcercise/SingleExcercise.js
@@ -5,11 +5,11 @@ const SingleExcercise = (props) => {
     const { name, image, desc, time } = props.excercise;
     return (
         <div className='bg-white p-5 rounded-lg'>
-            <img src={image} alt="" />
-            <h4>{name}</h4>
-            <p>{ desc }</p>
-            <p>Time Required: {time}</p>
-            <button>Add to List</button>
+            <img className='h-48 w-full rounded-lg' src={image} alt="" />
+            <h4 className='text-2xl font-semibold my-3'>{name}</h4>
+            <p className='my-1'>{ desc }</p>
+            <p className='my-1'>Time Required: <span className='font-semibold'>{time}</span></p>
+            <button className='w-full bg-amber-800 py-2 rounded-lg mt-3 hover:bg-amber-700'>Add to List</button>
         </div>
     );
 };
