782e4b1bf652d4ec38de167a70be2be66280a486
theMorshed <themorshedctg@gmail.com>
2022-09-29 16:49:42 +0600

    break time component updated

--- src/components/BreakTime/BreakTime.js
+++ src/components/BreakTime/BreakTime.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './BreakTime.css';
 
 const BreakTime = ({ handleBreakTime }) => {
+    const breakTimes = [10, 20, 30, 40, 50];
     return (
         <div className='mt-8'>
             <h2 className='text-xl font-semibold'>Add a Break</h2>
             <div className='flex bg-amber-50 px-6 py-4 mt-8 rounded-lg justify-between'>
-                <p onClick={() => { handleBreakTime(10) }} className='p-2 rounded-lg bg-amber-800 hover:bg-amber-600 text-white'>10m</p>
-                <p onClick={() => { handleBreakTime(20) }} className='p-2 rounded-lg bg-amber-800 hover:bg-amber-600 text-white'>20m</p>
-                <p onClick={() => { handleBreakTime(30) }} className='p-2 rounded-lg bg-amber-800 hover:bg-amber-600 text-white'>30m</p>
-                <p onClick={() => { handleBreakTime(40) }} className='p-2 rounded-lg bg-amber-800 hover:bg-amber-600 text-white'>40m</p>
-                <p onClick={() => { handleBreakTime(50) }} className='p-2 rounded-lg bg-amber-800 hover:bg-amber-600 text-white'>50m</p>
+                {
+                    breakTimes.map(breakTime => <p key={breakTime} onClick={() => { handleBreakTime(breakTime) }} className='p-2 rounded-lg bg-amber-800 hover:bg-amber-600 text-white'>{breakTime}m</p>)
+                }
             </div>
-        </div>        
+        </div>
     );
 };
 
